2a54aef60bb17b3beec63c2bee4f0e77f13bca22
joshuaPhong <klx2044@yahoo.com>
2023-04-18 14:21:49 +1200

    python

--- Loops/Else with loops/else_with_loops.py
+++ Loops/Else with loops/else_with_loops.py
@@ -3,11 +3,14 @@ while i < 5:
     print('it\'s less than 5')
     i += 1
 else:
-    print('and now it\'s 5')  # Print this message when the condition is no longer True
+    print('and now it\'s 5')  # Print this message when the condition is no
+    # longer True
 
 for i in range(1, 5):
     # Add a condition for interruption.
+    if i == 3:
         # Add a statement that will terminate the loop.
+        break
     print(i)
 else:
     print("for loop is done")
--- Loops/Fix infinite execution/infinite.py
+++ Loops/Fix infinite execution/infinite.py
@@ -1,10 +1,9 @@
 def should_not_be_infinite():  # Function definition. Do not mind it for now.
-    i = 5
-
-    while i < 15:
+    i = 0
+    while i < 5:
         print("Hello, World!")
         # TODO
-
+        i += 1
 
 # The main block. Please do not mind it at the moment. You will learn about it later in the course.
 if __name__ == "__main__":

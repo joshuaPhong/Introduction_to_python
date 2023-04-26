58537b9f407bd2f55a06a26989ccce8b6f30285f
joshuaPhong <klx2044@yahoo.com>
2023-04-26 14:31:37 +1200

    python

--- Modules and packages/From import/from_import.py
+++ Modules and packages/From import/from_import.py
@@ -1,14 +1,12 @@
-# Import class Calculator from another module
+from calculator import Calculator
 from my_module import hello as hey
 
 print(hey("User"))
 
-calc = 'Instantiate a calculator'  # Name `Calculator` used directly without prefix `calculator`
+calc = Calculator()  # Name `Calculator` used directly without prefix
+# `calculator`
 calc.add(2)
 calc.multiply(100)
 calc.divide(3)
 
 print(calc.get_current())
-
-
-
